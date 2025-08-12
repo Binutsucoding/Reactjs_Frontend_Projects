6f0c2cf8e8a57488ca341b7704b51a8460f1e8ad
utsav shah <utsavshah972@gmail.com>
2025-08-12 18:13:19 +0530

    Delete 7-social-media/Reactjs_Frontend_Projects directory

--- 7-social-media/Reactjs_Frontend_Projects/src/Components/WelcomeMessage.jsx
+++
@@ -1,8 +0,0 @@
-const WelcomeMessage = () => {
-  return (
-    <center style={{ margin: "200px 300px 0 350px" }}>
-      <h5>There Are No Posts Yet Plz Add One!</h5>
-    </center>
-  );
-};
-export default WelcomeMessage;
